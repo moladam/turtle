ae0c41ff6f9ba8607afafa7cc20b1ba5b220da72
moladam <71643219+moladam@users.noreply.github.com>
2020-09-21 15:05:27 +0200

    Create 5szog.py

---
+++ 5szog.py
@@ -0,0 +1,15 @@
+import turtle               
+wn = turtle.Screen()        
+alex = turtle.Turtle()  
+h=75
+sz=72
+alex.forward(h)           
+alex.left(sz)               
+alex.forward(h)            
+alex.left(sz)
+alex.forward(h)
+alex.left(sz)
+alex.forward(h)
+alex.left(sz)
+alex.forward(h)
+alex.left(sz)
